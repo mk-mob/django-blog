8a9dd6be152d1060617dc047d02ae787a787fb16
mk-mob <71954702+mk-mob@users.noreply.github.com>
2024-01-22 16:40:12 +0900

    Update settings.py

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -16,10 +16,10 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
